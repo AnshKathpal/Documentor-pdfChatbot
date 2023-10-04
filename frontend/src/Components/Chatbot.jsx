36b6024e201ebbc3a623ae00e680c4db44b6c162
AnshKathpal <storage.2.3107@gmail.com>
2023-10-05 03:18:33 +0530

    frontend-design

--- frontend/src/Components/Chatbot.jsx
+++ frontend/src/Components/Chatbot.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { Box, Button, Input, Text } from "@chakra-ui/react";
+import { Box, Button, Input, Text, Flex } from "@chakra-ui/react";
 import { useState } from "react";
 import axios from "axios";
-import { IoCloudUpload } from "react-icons/io5";
+import { FcUpload } from "react-icons/fc";
 import styled, { keyframes, css } from "styled-components";
 
 export const Chatbot = () => {
@@ -11,9 +11,10 @@ export const Chatbot = () => {
   const [chooseName, setChooseName] = useState(null);
   const [userMessage, setUserMessage] = useState("");
   const [chatResult, setChatResult] = useState([]);
+  const [isChatFormVisible, setIsChatFormVisible] = useState(false);
 
   console.log(isAnimationActive);
-  console.log(selectedFile);
+  console.log(selectedFile, "selectedFile");
   console.log(chooseName);
   console.log("res", chatResult);
 
@@ -26,8 +27,9 @@ export const Chatbot = () => {
       }, 0);
 
       setTimeout(() => {
-        setChooseName("File Uploaded")
-      },7000)
+        setChooseName("File Uploaded");
+        setIsChatFormVisible(true);
+      }, 7000);
       const formData = new FormData();
       formData.append("file", selectedFile);
       try {
@@ -50,7 +52,7 @@ export const Chatbot = () => {
   const handleFileChange = (e) => {
     const file = e.target.files[0];
     setSelectedFile(file);
-      setChooseName(file.name);
+    setChooseName(file.name);
   };
 
   const handleSubmitChat = async (e) => {
@@ -79,17 +81,49 @@ export const Chatbot = () => {
   };
 
   return (
-    <>
-      <Text fontSize={"6xl"}>Study Ease</Text>
+    <Flex h="100vh" bg="#F8F0E5">
+      <Flex
+        pos="relative"
+        bg="#0F2C59"
+        p="20px"
+        w="30%"
+        direction={"column"}
+        align={"center"}
+      >
+        <Text
+          fontSize={"6xl"}
+          color="#DAC0A3"
+          fontFamily={"'Lilita One', cursive"}
+        >
+          DocuMentor
+        </Text>
 
-      <Box border = "1px solid red" >
-        <form action="" onSubmit={handleSubmit} style = {{border : "1px solid green" , width : "20%", margin : "auto"}} >
-          <Text pos = "absolute" left = "50%" top = "52%" transform={"translate(-50%)"}>{chooseName ? chooseName : "Choose File"}</Text>
-          <LoadingStyle
-            isAnimationActive={isAnimationActive}
+        <form action="" onSubmit={handleSubmit} style={{ margin: "auto" }}>
+          <Text
+            pos="absolute"
+            left="50%"
+            top="50%"
+            transform={"translate(-50%)"}
+            zIndex="3"
+            fontSize={"xl"}
+            fontFamily={"'Lilita One', cursive"}
           >
+            {chooseName ? chooseName : "Choose File"}
+          </Text>
+          <LoadingStyle isAnimationActive={isAnimationActive}>
+            <Box
+              pos={"absolute"}
+              left="50%"
+              top="5%"
+              transform="translate(-50%)"
+              borderRadius="30%"
+              border="2px dashed #0F2C59"
+              w="90%"
+              h="90%"
+              m="auto"
+            ></Box>
             <Input
-              border="1px solid red"
+              // border="1px solid red"
               opacity="0"
               type="file"
               w="100%"
@@ -100,21 +134,72 @@ export const Chatbot = () => {
               cursor="pointer"
             />
           </LoadingStyle>
-          <Button mt = "10px" type="submit">Upload</Button>
+          <Button mt="10px" type="submit">
+            Upload
+          </Button>
         </form>
-      </Box>
+      </Flex>
 
-      {/* <Box border="1px solid red" h="100px" w="70%" m="auto">
-        <form action="" onSubmit={handleSubmitChat}>
-          <Input
-            type="text"
-            value={userMessage}
-            onChange={(e) => setUserMessage(e.target.value)}
-          />
-          <Input type="submit" />
-        </form>
-      </Box> */}
-    </>
+      <Box bg="#EADBC8" w="60%" h="70vh" m="auto" borderRadius="30px">
+        {isChatFormVisible && (
+          <form
+            onSubmit={handleSubmitChat}
+            action=""
+            style={{ padding: "20px", height: "20%" }}
+          >
+            <Input
+              border="1px solid black"
+              h="50px"
+              fontSize={"xl"}
+              type="text"
+              value={userMessage}
+              onChange={(e) => setUserMessage(e.target.value)}
+              textAlign="center"
+              placeholder="Ask from your PDF here.."
+            />
+            <Button mt="15px" type="submit">
+              Submit
+            </Button>
+          </form>
+        )}
+        <Box h="80%" overflowY="scroll">
+          {chatResult.map((message, index) => (
+            <div
+              key={index}
+              className={`message-container ${
+                message.role === "user" ? "user-message" : "bot-message"
+              }`}
+              style={{
+                display: "flex",
+                justifyContent: `${
+                  message.role == "user" ? "flex-end" : "flex-start"
+                }`,
+                width: "100%",
+                padding: "10px 30px 10px 30px",
+              }}
+            >
+              <div
+                className={`message-container ${
+                  message.role === "user" ? "user-message" : "bot-message"
+                }`}
+                style={{
+                  width: `${message.role == "user" ? "30%" : "70%"}`,
+                  backgroundColor: "#0F2C59",
+                  color: "white",
+                  padding: "10px",
+                  borderRadius: "30px",
+                  boxShadow:
+                    "rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset",
+                }}
+              >
+                <strong>{message.role === "user" ? "You" : "Bot"}:</strong>{" "}
+                {message.content}
+              </div>
+            </div>
+          ))}
+        </Box>
+      </Box>
+    </Flex>
   );
 };
 
@@ -134,25 +219,26 @@ const fill = keyframes`
 
 const LoadingStyle = styled.div`
 
-border : 1px solid red;
-width : 160px;
-height : 160px;
-border-radius : 50%;
+width : 200px;
+height : 200px;
+border-radius : 30%;
 margin : auto;
 position : relative;
 overflow : hidden;
+background-color : #DAC0A3;
+z-index : 2;
 
 &:before{
   content : "";
   position : absolute;
-  width : 300px;
-  height : 300px;
+  width : 400px;
+  height : 400px;
   background-color : #00acee;
   left : 50%;
   transform : translateX(-50%);
   top : 200px;
   border-radius : 40%;
-  z-index : -10;
+  z-index : -2;
   animation: ${(props) =>
     props.isAnimationActive
       ? css`
